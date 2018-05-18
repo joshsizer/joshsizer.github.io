687438cdd76bc2de9eb26e4661026d3715baf898
Josh Sizer <joshsizer@gmail.com>
2018-05-18 11:58:47 -0400

    Updated post High School.

--- javascript/footer.js
+++ javascript/footer.js
@@ -8,7 +8,7 @@ function createFooterString() {
    return "<footer class=\"footer\">" +
                 "<div class=\"container\">" +
                     "<div class=\"footer-flex\">" +
-                        "<span>&copy; 2017 Joshua Sizer</span>"	+
+                        "<span>&copy; 2018 Joshua Sizer</span>"	+
                         "<span>Contact: joshsizer@gmail.com<span>" +                 
                     "</div>" +
                 "</div>" +
